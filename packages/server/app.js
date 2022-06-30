87d7cc4125cdf17f879e401b2fa0bd86ffb6d133
Wesley Ford <wes@wesdev.pro>
2022-06-30 17:32:30 -0500

    Configured server for heroku
    I have added the PROCFILE for Heroku. And I have made a little API that will serve up an env var.

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,2 +1,14 @@
-require('dotenv').config();
-let test = process.env.HELLO_WORLD;
+import dotenv from 'dotenv'
+import express from 'express'
+dotenv.config()
+const test = process.env.HELLO_WORLD
+const app = express()
+const port =  process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send(test)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
