de378856ce23cb74470cc4e6f0f93abba8201224
Wesley Ford <wes@wesdev.pro>
2022-07-12 12:37:18 -0400

    Updated Fontawesome
    This should fix the bug

--- packages/client/nuxt.config.js
+++ packages/client/nuxt.config.js
@@ -18,9 +18,7 @@ export default {
   loading: { color: '#fff' },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/scss/main.scss',
-  ],
+  css: ['~/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
