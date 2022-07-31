75460702c34142827a1eb968cb130c7538ce140b
dansmalr1527 <108247048+dansmalr1527@users.noreply.github.com>
2022-07-30 22:27:05 -0400

    UI (#10)
    * UI changes; footer needs work
    
    UI changes; footer needs work
    
    * UI changes
    
    UI; footer needs work
    
    * UI changes
    
    UI; footer needs work

--- packages/client/layouts/default.vue
+++ packages/client/layouts/default.vue
@@ -1,5 +1,21 @@
+<script>
+import click from '../components/click'
+import foot from '../components/foot'
+
+export default {
+  components: {
+    click,
+    foot
+  }
+}
+
+</script>
+
 <template>
   <section class="section">
+    <top-nav />
+    <side-nav />
     <nuxt />
+    <foot />
   </section>
 </template>
