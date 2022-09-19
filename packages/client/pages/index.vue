219dd190436ca14ccc4dc974779eee0e2b5c85fc
Wesley Ford <wes@wesdev.pro>
2022-09-18 21:59:24 -0500

    Update index.vue

--- packages/client/pages/index.vue
+++ packages/client/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="section">
+    <img src="@/assets/education_trail_emblem.png" style="height: 150px" />
+    <p>Please excuse our dust. There is a bear on the trail🐻</p>
     <p>We are still working on the education trail!</p>
     <br>
     <p>Find us on <a href="https://www.instagram.com/educationtrail/">instagram</a></p>
