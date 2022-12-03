504f1af51624ed685114d4f8cef1ffdba53f6468
Wesley Ford <wes@wesdev.pro>
2022-12-03 13:40:56 -0600

    Content News

---
+++ src/client/components/content/release.vue
@@ -0,0 +1,9 @@
+<template>
+   <div class="section">
+      <img src="/assets/education_trail_emblem.png" style="height: 150px" />
+      <p>Please excuse our dust. There is a bear on the trail🐻</p>
+      <p>We are still working on the education trail!</p>
+      <br>
+      <p>Find us on <a class="has-text-link" href="https://www.instagram.com/educationtrail/">instagram</a></p>
+   </div>
+</template>
