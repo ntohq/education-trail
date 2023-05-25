461490e4339b20ec84c430356d832b042e858257
Wesley Ford <wes@wesdev.pro>
2023-05-25 02:29:07 -0400

    Migrated Style Framework to Tailwind and DaisyUI

--- src/client/content/news/release-date.md
+++
@@ -1,9 +0,0 @@
----
-title: Release Date?
-clentcher: When will The Education Trail Launch?
-category: Launch Sequence
-postedAt: 2022-12-01T21:19:19.297Z
-img: "https://images.pexels.com/photos/355906/pexels-photo-355906.jpeg?cs=srgb&dl=pexels-pixabay-355906.jpg&fm=jpg&_gl=1*1606sf4*_ga*MTc0ODUwNzg3My4xNjY2OTAxNjcx*_ga_8JE65Q40S6*MTY3MDA4NjE3My4xMy4xLjE2NzAwODYxOTAuMC4wLjA."
----
-
-::release
--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,18 +1,13 @@
 <template>
-    <div class="layout-container">
-        <PublicHomeHero></PublicHomeHero>
-        <PublicHomeAbout></PublicHomeAbout>
-        <Countdown></Countdown>
-        <PublicOurTeam></PublicOurTeam>
-        <PublicPricing></PublicPricing>
+    <div class="hero min-h-screen" style="background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+        <div class="hero-overlay s-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+                <h1 class="mb-5 text-5xl font-bold"><span class="i-mdi-home hover:i-mdi-home-alert text-5xl"></span>Hello there</h1>
+                <p class="mb-5">They provide pleasure in desire and in. He sought to escape, as if he were to be expected to receive training. 
+                    In the softening of them, or of repudiating them, and from that unless.</p>
+                <button class="btn btn-primary">Get Started</button>
+            </div>
+        </div>
     </div>
 </template>
-
-<script setup>
-useHead({
-    title: 'Education Trail - Home',
-    meta: [
-        { name: 'description', content: 'The home of the education trail! The Education trail is a powerful tool that makes grade analytics simple-created to help anyone to reach their education goals.' },
-    ]
-})
-</script>
