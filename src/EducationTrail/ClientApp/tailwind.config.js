ef11117dc201c9443d0de7f85a5e9e109544cc5d
Wesley Ford <wes@wesdev.pro>
2023-07-14 23:00:15 -0400

    Added Icons and Typography Plugins

--- src/EducationTrail/ClientApp/tailwind.config.js
+++ src/EducationTrail/ClientApp/tailwind.config.js
@@ -1,7 +1,19 @@
+const {
+  iconsPlugin,
+  getIconCollections,
+} = require("@egoist/tailwindcss-icons");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [require("daisyui")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    iconsPlugin({
+      // Select the icon collections you want to use
+      collections: getIconCollections(["mdi", "uil"]),
+    }),
+  ],
   daisyui: {
     themes: [
       {
