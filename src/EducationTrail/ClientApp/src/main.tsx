5d109309600bf6098b4e34ac94de8c2989754ae3
Wesley Ford <wes@wesdevpro.com>
2023-11-20 16:02:46 -0600

    chore(spa): upgrade to the latest version of react

--- src/EducationTrail/ClientApp/src/main.tsx
+++ src/EducationTrail/ClientApp/src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM  from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App'
 import '@assets/css/core.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
